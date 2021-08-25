d6ce111ee5f9a7d75e7ba193c498c380f9bf1d89
Franco Ramirez <francoramirezreyes@gmail.com>
2021-08-25 05:36:45 -0500

    purge safelist for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: {
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    safelist: [
+      'bg-red-500',
+      'bg-red-400',
+      'hover:bg-red-500',
+      'bg-green-400',
+      'hover:bg-green-500',
+      'bg-yellow-400',
+      'hover:bg-yellow-500',
+      'bg-gray-200',
+      'hover:bg-gray-300',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
